7053cac239a265f962a622176268403f3a35768c
Omar Letaief <94861909+OmarLtf@users.noreply.github.com>
2023-04-28 15:51:49 +0100

    Delete ProductRepo.java

--- smrBackend/src/main/java/com/example/springbootmongodbatlas/repo/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootmongodbatlas.repo;
-
-import com.example.springbootmongodbatlas.entity.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepo extends MongoRepository<Product,Integer> {
-}
